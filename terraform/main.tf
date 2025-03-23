31589465e673660b98b7d4a48f16426935b130e3
Will Yip <will@cloudful.io>
2025-03-22 22:11:19 -0400

    Updated Terraform to set default root object of CloudFront distribution

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,22 +2,31 @@ module "s3-bucket" {
     source = "github.com/cloudful-io/terraform-aws-s3"
   
     bucket_name             = "cloudful-react-deployment-s3"
-    static_website_hosting  = true
-    block_public_access     = false
+    static_website_hosting  = false
+    block_public_access     = true
     create_kms_key          = false
     create_logging_bucket   = false
     logging_bucket_name     = "cloudful-logs"
 }
 
-/*
+resource "aws_cloudfront_origin_access_control" "origin-access-control" {
+  name                              = module.s3-bucket.bucket_regional_domain_name
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
 # Create CloudFront Distribution
 resource "aws_cloudfront_distribution" "cloudfront-distribution" { 
   origin {
     domain_name = module.s3-bucket.bucket_regional_domain_name
     origin_id   = "S3-${module.s3-bucket.id}"
+    origin_access_control_id = aws_cloudfront_origin_access_control.origin-access-control.id
   }
-
+  
+  default_root_object = "index.html"
   enabled = true
+
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
@@ -42,7 +51,28 @@ resource "aws_cloudfront_distribution" "cloudfront-distribution" {
     }
   }
 }
-*/
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = module.s3-bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "AllowCloudFrontServicePrincipal",
+        Effect    = "Allow",
+        Principal = { Service = "cloudfront.amazonaws.com" },
+        Action    = "s3:GetObject",
+        Resource  = "arn:aws:s3:::${module.s3-bucket.id}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.cloudfront-distribution.arn
+          }
+        }
+      }
+    ]
+  })
+}
+
 # IAM User for GitHub Actions
 resource "aws_iam_user" "github_deploy_user" {
   name = "github-deploy-user"
@@ -83,3 +113,4 @@ resource "aws_iam_user_policy_attachment" "attach_policy" {
 resource "aws_iam_access_key" "github_deploy_key" {
   user = aws_iam_user.github_deploy_user.name
 }
+

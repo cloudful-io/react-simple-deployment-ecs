03b4b8e2fef34e27b1eb8706033a79a5c67471d2
Will Yip <will@cloudful.io>
2025-04-04 22:54:55 -0400

    Updated Github Action definition

--- terraform/outputs.tf
+++
@@ -1,8 +0,0 @@
-# Output Values
-output "s3_bucket_name" {
-  value = module.s3-bucket.id
-}
-
-output "cloudfront_distribution_url" {
-  value = aws_cloudfront_distribution.cloudfront-distribution.domain_name
-}
